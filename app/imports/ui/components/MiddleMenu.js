9d60b8e58e09d0015137c1cfab56e0fd7c73086a
Willard Peralta <willardp.peralta@gmail.com>
2018-11-13 10:56:04 -1000

    landing draft

--- app/imports/ui/components/MiddleMenu.js
+++ app/imports/ui/components/MiddleMenu.js
@@ -1,31 +1,14 @@
 import React from 'react';
-import { Menu, Dropdown, Grid } from 'semantic-ui-react';
+import { Menu, Grid } from 'semantic-ui-react';
 
 export default class MiddleMenu extends React.Component {
   render() {
     return (
         <Menu borderless className="middle">
           <Grid container centered>
-            <Dropdown item text="MEN" icon="dropdown icon">
-              <Dropdown.Menu>
-                <Dropdown.Item>Tank tops</Dropdown.Item>
-                <Dropdown.Item>Shirts</Dropdown.Item>
-              </Dropdown.Menu>
-            </Dropdown>
-            <Dropdown item text="WOMEN" icon="dropdown icon">
-              <Dropdown.Menu>
-                <Dropdown.Item>Tank tops</Dropdown.Item>
-                <Dropdown.Item>Shirts</Dropdown.Item>
-              </Dropdown.Menu>
-            </Dropdown>
-            <Menu.Item fitted>KIDS</Menu.Item>
-            <Dropdown item text="BRANDS" icon="dropdown icon">
-              <Dropdown.Menu>
-                <Dropdown.Item>Tank tops</Dropdown.Item>
-                <Dropdown.Item>Shirts</Dropdown.Item>
-              </Dropdown.Menu>
-            </Dropdown>
-            <Menu.Item fitted>SEARCH</Menu.Item>
+            <Menu.Item>Profiles</Menu.Item>
+            <Menu.Item>Listings</Menu.Item>
+            <Menu.Item>Contact</Menu.Item>
           </Grid>
         </Menu>
     );
