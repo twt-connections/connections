e1c89f22b43d7c2a54cf4ad5bf699389549ee109
Willard Peralta <willardp.peralta@gmail.com>
2018-11-18 16:31:26 -1000

    Update CompanyHome.jsx

--- app/imports/ui/pages/CompanyHome.jsx
+++ app/imports/ui/pages/CompanyHome.jsx
@@ -17,93 +17,99 @@ class CompanyHome extends React.Component {
   /** Render the page once subscriptions have been received. */
   renderPage() {
     return (
-        <Container>
-          <Header as="h2" textAlign="center">Your Connections</Header>
-          <Card.Group centered>
-            <Card>
-              <Card.Content>
-                <Image floated='right' size='mini' src='https://react.semantic-ui.com/images/avatar/large/steve.jpg' />
-                <Card.Header>Steve Sanders</Card.Header>
-                <Card.Meta>UH Manoa, BS Computer Science 2019</Card.Meta>
-                <Card.Description>
-                  Hello, I'm Steve, a recent graduate of UH Manoa. I am currently looking for a job in software engineering. <br/>
-                  <b>Location</b>: Honolulu, HI <br/>
-                  <b>Skillset</b>: Java, C++ <br/>
-                  <b>Interests</b>: Software Engineering, Web Design <br/>
-                  <b>Experience</b>: Entry-level
-                </Card.Description>
-              </Card.Content>
-              <Card.Content extra>
-                <div className='ui three buttons'>
-                  <Button basic color='green'>
-                    Request Info
-                  </Button>
-                  <Button basic color='blue'>
-                    Contact
-                  </Button>
-                  <Button basic color='red'>
-                    Remove
-                  </Button>
-                </div>
-              </Card.Content>
-            </Card>
-            <Card>
-              <Card.Content>
-                <Image floated='right' size='mini' src='https://react.semantic-ui.com/images/avatar/large/molly.png' />
-                <Card.Header>Molly Thomas</Card.Header>
-                <Card.Meta>UH Manoa, BS Computer Science 2020</Card.Meta>
-                <Card.Description>
-                  Hey! My name is Molly Thomas, I'm currently a student at UH Manoa in Hawaii, majoring in Computer Science. I'm seeking an internship for next summer. <br/>
-                  <b>Location</b>: Honolulu, HI <br/>
-                  <b>Skillset</b>: C, Javascript <br/>
-                  <b>Interests</b>: Artificial Intelligence, Databases <br/>
-                  <b>Experience</b>: Entry-level
-                </Card.Description>
-              </Card.Content>
-              <Card.Content extra>
-                <div className='ui three buttons'>
-                  <Button basic color='green'>
-                    Request Info
-                  </Button>
-                  <Button basic color='blue'>
-                    Contact
-                  </Button>
-                  <Button basic color='red'>
-                    Remove
-                  </Button>
-                </div>
-              </Card.Content>
-            </Card>
-            <Card>
-              <Card.Content>
-                <Image floated='right' size='mini' src='https://react.semantic-ui.com/images/avatar/large/elliot.jpg' />
-                <Card.Header>Elliot Baker</Card.Header>
-                <Card.Meta>UH Manoa, MS Mathematics 1996</Card.Meta>
-                <Card.Description>
-                  GREETINGS, I AM ELLIOT BAKER. DO TO UNSAVORY CIRCUMSTANCES, I AM CURRENTLY SEEKING A CHANGE OF EMPLOYMENT. <br/>
-                  <b>Location</b>: HONOLULU, HI, 96782 <br/>
-                  <b>Skillset</b>: ALGOL-68, COBOL, LINUX, AND ALL OBJECT-ORIENTED LANGUAGES. <br/>
-                  <b>Interests</b>: ASTROPHYSICS <br/>
-                  <b>Experience</b>: 20+ YEARS
-                </Card.Description>
-              </Card.Content>
-              <Card.Content extra>
-                <div className='ui three buttons'>
-                  <Button basic color='green'>
-                    Request Info
-                  </Button>
-                  <Button basic color='blue'>
-                    Contact
-                  </Button>
-                  <Button basic color='red'>
-                    Remove
-                  </Button>
-                </div>
-              </Card.Content>
-            </Card>
-          </Card.Group>
-          <br/>
-        </Container>
+        <div className="landing-background-image">
+          <Container>
+            <Header as="h2" textAlign="center">Your Connections</Header>
+            <Card.Group centered>
+              <Card>
+                <Card.Content>
+                  <Image floated='right' size='mini' src='https://react.semantic-ui.com/images/avatar/large/steve.jpg'/>
+                  <Card.Header>Steve Sanders</Card.Header>
+                  <Card.Meta>UH Manoa, BS Computer Science 2019</Card.Meta>
+                  <Card.Description>
+                    Hello, I'm Steve, a recent graduate of UH Manoa. I am currently looking for a job in software
+                    engineering. <br/>
+                    <b>Location</b>: Honolulu, HI <br/>
+                    <b>Skillset</b>: Java, C++ <br/>
+                    <b>Interests</b>: Software Engineering, Web Design <br/>
+                    <b>Experience</b>: Entry-level
+                  </Card.Description>
+                </Card.Content>
+                <Card.Content extra>
+                  <div className='ui three buttons'>
+                    <Button basic color='green'>
+                      Request Info
+                    </Button>
+                    <Button basic color='blue'>
+                      Contact
+                    </Button>
+                    <Button basic color='red'>
+                      Remove
+                    </Button>
+                  </div>
+                </Card.Content>
+              </Card>
+              <Card>
+                <Card.Content>
+                  <Image floated='right' size='mini' src='https://react.semantic-ui.com/images/avatar/large/molly.png'/>
+                  <Card.Header>Molly Thomas</Card.Header>
+                  <Card.Meta>UH Manoa, BS Computer Science 2020</Card.Meta>
+                  <Card.Description>
+                    Hey! My name is Molly Thomas, I'm currently a student at UH Manoa in Hawaii, majoring in Computer
+                    Science. I'm seeking an internship for next summer. <br/>
+                    <b>Location</b>: Honolulu, HI <br/>
+                    <b>Skillset</b>: C, Javascript <br/>
+                    <b>Interests</b>: Artificial Intelligence, Databases <br/>
+                    <b>Experience</b>: Entry-level
+                  </Card.Description>
+                </Card.Content>
+                <Card.Content extra>
+                  <div className='ui three buttons'>
+                    <Button basic color='green'>
+                      Request Info
+                    </Button>
+                    <Button basic color='blue'>
+                      Contact
+                    </Button>
+                    <Button basic color='red'>
+                      Remove
+                    </Button>
+                  </div>
+                </Card.Content>
+              </Card>
+              <Card>
+                <Card.Content>
+                  <Image floated='right' size='mini'
+                         src='https://react.semantic-ui.com/images/avatar/large/elliot.jpg'/>
+                  <Card.Header>Elliot Baker</Card.Header>
+                  <Card.Meta>UH Manoa, MS Mathematics 1996</Card.Meta>
+                  <Card.Description>
+                    GREETINGS, I AM ELLIOT BAKER. DO TO UNSAVORY CIRCUMSTANCES, I AM CURRENTLY SEEKING A CHANGE OF
+                    EMPLOYMENT. <br/>
+                    <b>Location</b>: HONOLULU, HI, 96782 <br/>
+                    <b>Skillset</b>: ALGOL-68, COBOL, LINUX, AND ALL OBJECT-ORIENTED LANGUAGES. <br/>
+                    <b>Interests</b>: ASTROPHYSICS <br/>
+                    <b>Experience</b>: 20+ YEARS
+                  </Card.Description>
+                </Card.Content>
+                <Card.Content extra>
+                  <div className='ui three buttons'>
+                    <Button basic color='green'>
+                      Request Info
+                    </Button>
+                    <Button basic color='blue'>
+                      Contact
+                    </Button>
+                    <Button basic color='red'>
+                      Remove
+                    </Button>
+                  </div>
+                </Card.Content>
+              </Card>
+            </Card.Group>
+            <br/>
+          </Container>
+        </div>
     );
   }
 }
