8e4753c9c36f046b5ecaab57e9cea8867b7d2d39
Ty Gwartney <tyg@hawaii.edu>
2018-11-24 23:01:34 -1000

    Company Profile Simple Schema, Collection
    Defined a Simple Schema for Company profiles, and added a CompanyProfiles collection.

---
+++ app/imports/api/profiles/CompanyProfile.js
@@ -0,0 +1,21 @@
+import { Mongo } from 'meteor/mongo';
+import SimpleSchema from 'simpl-schema';
+import { Tracker } from 'meteor/tracker';
+
+/** Create a Meteor collection. */
+const CompanyProfiles = new Mongo.Collection('CompanyProfiles');
+
+/** Create a schema to constrain the structure of documents associated with this collection. */
+const CompanyProfileSchema = new SimpleSchema({
+name: String,
+image: String,
+location: String,
+description: String,
+owner: String,
+}, { tracker: Tracker });
+
+/** Attach this schema to the collection. */
+CompanyProfiles.attachSchema(CompanyProfileSchema);
+
+/** Make the collection and schema available to other code. */
+export { CompanyProfiles, CompanyProfileSchema };
