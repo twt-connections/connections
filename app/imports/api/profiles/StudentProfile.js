20e0a3209caf95f6414329c34353b50009ad79d7
Willard Peralta <willardp.peralta@gmail.com>
2018-11-27 10:32:16 -1000

    Update StudentProfile.js

--- app/imports/api/profiles/StudentProfile.js
+++ app/imports/api/profiles/StudentProfile.js
@@ -6,17 +6,17 @@ import { Tracker } from 'meteor/tracker';
 const StudentProfiles = new Mongo.Collection('StudentProfiles');
 
 /** Create a schema to constrain the structure of documents associated with this collection. */
-const ProfileSchema = new SimpleSchema({
-image: String,
-firstName: String,
-lastName: String,
-degree: String,
-school: String,
-owner: String,
+const StudentProfileSchema = new SimpleSchema({
+  image: String,
+  firstName: String,
+  lastName: String,
+  degree: String,
+  school: String,
+  owner: String,
 }, { tracker: Tracker });
 
 /** Attach this schema to the collection. */
-StudentProfiles.attachSchema(ProfileSchema);
+StudentProfiles.attachSchema(StudentProfileSchema);
 
 /** Make the collection and schema available to other code. */
-export { StudentProfiles, ProfileSchema };
+export { StudentProfiles, StudentProfileSchema };
