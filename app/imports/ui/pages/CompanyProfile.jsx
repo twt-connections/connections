de22043283689d7b25cfcc057268565063f32bd7
tregelacio <tre.gelacio@gmail.com>
2018-11-27 11:28:32 -1000

    Changed CompanyProfiles

--- app/imports/ui/pages/CompanyProfile.jsx
+++ app/imports/ui/pages/CompanyProfile.jsx
@@ -1,176 +1,45 @@
 import React from 'react';
-import { CompanyProfiles, CompanyProfileSchema } from '/imports/api/profiles/CompanyProfile';
-import { Header, Image, Container, Button, Segment, Grid } from 'semantic-ui-react';
-import { Bert } from 'meteor/themeteorchef:bert';
 import { Meteor } from 'meteor/meteor';
+import { Container, Header, Loader, Card } from 'semantic-ui-react';
+import { CompanyProfiles } from '/imports/api/profiles/CompanyProfile';
+import { withTracker } from 'meteor/react-meteor-data';
+import PropTypes from 'prop-types';
+import CompanyItem from '/imports/ui/components/CompanyItem';
 
-/** Renders the Page for adding a document. */
+/** Renders a table containing all of the Stuff documents. Use <StuffItem> to render each row. */
 class CompanyProfile extends React.Component {
 
-  /** Bind 'this' so that a ref to the Form can be saved in formRef and communicated between render() and submit(). */
-  constructor(props) {
-    super(props);
-    this.submit = this.submit.bind(this);
-    this.insertCallback = this.insertCallback.bind(this);
-    this.formRef = null;
-  }
-
-  /** Notify the user of the results of the submit. If successful, clear the form. */
-  insertCallback(error) {
-    if (error) {
-      Bert.alert({ type: 'danger', message: `Add failed: ${error.message}` });
-    } else {
-      Bert.alert({ type: 'success', message: 'Add succeeded' });
-      this.formRef.reset();
-    }
-  }
-
-  /** On submit, insert the data. */
-  submit(data) {
-    const { name, image, slogan, location, description } = data;
-    const owner = Meteor.user().username;
-    CompanyProfiles.insert({ name, image, slogan, location, description, owner }, this.insertCallback);
-  }
-
-  /** Render the form. Use Uniforms: https://github.com/vazco/uniforms */
+  /** If the subscription(s) have been received, render the page, otherwise show a loading icon. */
   render() {
+    return (this.props.ready) ? this.renderPage() : <Loader active>Getting data</Loader>;
+  }
+
+  /** Render the page once subscriptions have been received. */
+  renderPage() {
     return (
+        <div className="landing-background-image">
           <Container>
-            <Header
-                as='h1'
-                content='Company Name'
-                style={{
-                  fontSize: '4em',
-                  fontWeight: 'normal',
-                  marginBottom: 0,
-                  marginTop: '3em',
-                  textAlign: 'center',
-                }}
-            />
-            <Header
-                as='h2'
-                content='Company Slogan'
-                style={{
-                  fontSize: '1.7em',
-                  fontWeight: 'normal',
-                  marginTop: '1.5em',
-                  textAlign: 'center',
-                }}
-            />
-
-            <Segment style={{ padding: '8em 0em' }} vertical>
-              <Grid container stackable verticalAlign='middle'>
-                <Grid.Row>
-                  <Grid.Column width={8}>
-                    <Header as='h2' style={{ fontSize: '2em' }}>
-                      All about our company
-                    </Header>
-                    <p style={{ fontSize: '1.33em' }}>
-                      We are good company. We have puppies :)
-                    </p>
-                    <Header as='h2' style={{ fontSize: '2em' }}>
-                      What we are looking for
-                    </Header>
-                    <p style={{ fontSize: '1.33em' }}>
-                      Something something we need to hire some people so just send us
-                      your resume and we'll hire you.
-                    </p>
-                  </Grid.Column>
-
-                  <Grid.Column floated='right' width={6}>
-                    <Image bordered rounded size='large'
-                           src='https://react.semantic-ui.com/images/wireframe/white-image.png'/>
-                  </Grid.Column>
-                </Grid.Row>
-
-                <Grid.Row>
-                  <Grid.Column textAlign='center'>
-                    <Button size='large'>Go to site</Button>
-                  </Grid.Column>
-                </Grid.Row>
-              </Grid>
-            </Segment>
-
-            <Segment style={{ padding: '0em' }} vertical>
-              <Grid celled='internally' columns='equal' stackable>
-                <Grid.Row textAlign='center'>
-                  <Grid.Column style={{ paddingBottom: '5em', paddingTop: '5em' }}>
-                    <Header as='h3' style={{ fontSize: '2m' }}>
-                      Starting positions available
-                    </Header>
-                    <p style={{ fontSize: '1.33em' }}>
-                      Front-End Developers
-                      <Button size='small' floated='right'>Click for specifications</Button>
-                    </p>
-                    <p style={{ fontSize: '1.33em' }}>
-                      Back-End Developers
-                      <Button size='small' floated='right'>Click for Specifications</Button>
-                    </p>
-                    <p style={{ fontSize: '1.33em' }}>
-                      Just need more devs honestly
-                      <Button size='small' floated='right'>Click for specifications</Button>
-                    </p>
-                  </Grid.Column>
-
-                  <Grid.Column style={{ paddingBottom: '5em', paddingTop: '5em' }}>
-                    <Header as='h3' style={{ fontSize: '2m' }}>
-                      What else do we offer
-                    </Header>
-                    <p style={{ fontSize: '1.25em' }}>
-                      Internships
-                    </p>
-                    <p style={{ fontSize: '1.25em' }}>
-                      Jobs (obviously)
-                    </p>
-                    <p style={{ fontSize: '1.25em' }}>
-                      Free food
-                    </p>
-                    <p style={{ fontSize: '1.25em' }}>
-                      AND PUPPIES
-                    </p>
-                  </Grid.Column>
-                </Grid.Row>
-              </Grid>
-            </Segment>
-
-            <Segment style={{ padding: '8em 0em' }} vertical>
-              <Grid container stackable verticalAlign='middle' columns='equal'>
-                <Grid.Row textAlign='center'>
-                  <Grid.Column style={{ paddingBottom: '2.5em', paddingTop: '2.5em' }}>
-                    <Header as='h3' style={{ fontSize: '2m' }}>
-                      Extra links
-                    </Header>
-                    <p style={{ fontSize: '1em' }}>
-                      <a src="www.sanger.dk">click here for our website</a>
-                    </p>
-                    <p style={{ fontSize: '1em' }}>
-                      <a src="www.sanger.dk">click here for money</a>
-                    </p>
-                    <p style={{ fontSize: '1em' }}>
-                      <a src="www.sanger.dk">click here if you want good grades</a>
-                    </p>
-                  </Grid.Column>
-
-                  <Grid.Column style={{ paddingBottom: '2.5em', paddingTop: '2.5em' }}>
-                    <Header as='h3' style={{ fontSize: '2m' }}>
-                      Contact emails
-                    </Header>
-                    <p style={{ fontSize: '1em' }}>
-                      <a src="www.sanger.dk">ceo@companyname.com</a>
-                    </p>
-                    <p style={{ fontSize: '1em' }}>
-                      <a src="www.sanger.dk">regionalmanager@companyname.com</a>
-                    </p>
-                    <p style={{ fontSize: '1em' }}>
-                      <a src="www.sanger.dk">assistanttotheregionalmanager@companyname.com</a>
-                    </p>
-                  </Grid.Column>
-                </Grid.Row>
-              </Grid>
-            </Segment>
+            <Header as="h2" textAlign="center">Company Profile Page</Header>
+            
+            <br/>
           </Container>
+        </div>
     );
   }
 }
 
-export default CompanyProfile;
+/** Require an array of Stuff documents in the props. */
+CompanyProfile.propTypes = {
+  companyprofiles: PropTypes.array.isRequired,
+  ready: PropTypes.bool.isRequired,
+};
+
+/** withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker */
+export default withTracker(() => {
+  // Get access to CompanyProfile documents.
+  const subscription = Meteor.subscribe('CompanyProfiles');
+  return {
+    companyprofiles: CompanyProfiles.find({}).fetch(),
+    ready: subscription.ready(),
+  };
+})(CompanyProfile);
