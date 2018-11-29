8fc8138d1b8fba5591a1993c3fe6d1ce0c187513
Willard Peralta <willardp808@gmail.com>
2018-11-29 11:42:18 -1000

    fix browse listings with companyItem

--- app/imports/ui/components/CompanyItem.jsx
+++ app/imports/ui/components/CompanyItem.jsx
@@ -10,21 +10,21 @@ class CompanyItem extends React.Component {
     return (
         <Card centered>
           <Card.Content>
-            <Image floated='right' size='mini' src={this.props.companyprofile.image} />
+            <Image floated='right' size='mini' src={this.props.companyItem.image} />
             <Card.Header>
-              {this.props.companyprofile.name}
+              {this.props.companyItem.name}
             </Card.Header>
             <Card.Meta>
-              {this.props.companyprofile.location}
+              {this.props.companyItem.location}
             </Card.Meta>
             <Card.Description>
-              {this.props.companyprofile.slogan}
+              {this.props.companyItem.slogan}
             </Card.Description>
             <Card.Description>
-              {this.props.companyprofile.description}
+              {this.props.companyItem.description}
             </Card.Description>
             <Card.Content extra>
-              <Link to={`/editCompanyProfile/${this.props.companyprofile._id}`}>Click to Edit</Link>
+              <Link to={`/editCompanyProfile/${this.props.companyItem._id}`}>Click to Edit</Link>
             </Card.Content>
           </Card.Content>
         </Card>
@@ -34,7 +34,7 @@ class CompanyItem extends React.Component {
 
 /** Require a document to be passed to this component. */
 CompanyItem.propTypes = {
-  companyprofile: PropTypes.object.isRequired,
+  companyItem: PropTypes.object.isRequired,
 };
 
 /** Wrap this component in withRouter since we use the <Link> React Router element. */
